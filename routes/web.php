9022eb93ff12c9adafc685bb6ae4cbc6d0c5a0bc
Farshad Nabizadeh <farshadn@hotelistan.com>
2023-01-12 04:24:29 -0800

    sitemap

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ include $__dir . 'Web_content/Webcontent.php';
 include $__dir . 'Users_and_Agencies/__USERS__AGENCIES.php';
 include $__dir . 'Ideas_and_Packages/Ideas_and_Packages.php';
 include $__dir . 'TripIdeasCategories/TripIdeasCategories.php';
+include $__dir . 'siteMap/siteMap.php';
